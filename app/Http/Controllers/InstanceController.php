65c11a1acbfe9f92e4cd92fe21e652c8c30b4938
Abdourahamane <amdroudjae@gmail.com>
2024-06-29 17:36:06 +0200

    Création du contrôleur InstanceController

---
+++ app/Http/Controllers/InstanceController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Instance;
+use App\Http\Requests\StoreInstanceRequest;
+use App\Http\Requests\UpdateInstanceRequest;
+
+class InstanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInstanceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Instance $instance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInstanceRequest $request, Instance $instance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Instance $instance)
+    {
+        //
+    }
+}
