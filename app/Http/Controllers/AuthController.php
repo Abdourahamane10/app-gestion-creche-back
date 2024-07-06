e93055cdf723452576d14184dfabb82ea26204e9
Abdourahamane <amdroudjae@gmail.com>
2024-07-06 17:12:04 +0200

    Installation de L5 Swagger et documentation de l'API POST/loginSociete

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,32 @@ class AuthController extends Controller
 {
     use ApiResponseTrait;
 
-
+    /**
+     * @OA\POST(
+     *     path="/api/loginSociete",
+     *     summary="Connecte l'utilisateur à la base de données de la société qui lui correspond",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"codeSociete", "codeService"},
+     *             @OA\Property(property="codeSociete", type="string", description="Code de connexion de la société"),
+     *             @OA\Property(property="codeService", type="string", description="Code de connexion du service de     l'utiilisateur")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Retourne un message qui indique que la connexion à la base de données a réussi",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="success", type="boolean", example=true),
+     *             @OA\Property(property="message", type="string", example="Connexion à la base de données réussie!"),
+     *             @OA\Property(property="data", type="string", example="")
+     *         )
+     *     )
+     * )
+     *
+     * @return void
+     */
     public function loginSociete()
     {
         return $this->successResponse("", "Connexion à la base de données réussie!");
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,19 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     version="1.0.1",
+ *     title="Documentation d'APIs",
+ *     description="Documentation des APIs de l'appplication de gestion de crèche"
+ * )
+ *
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Serveur"
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
