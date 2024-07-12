3655cde7892d619587f02a559de8bdc7316c73eb
Abdourahamane <amdroudjae@gmail.com>
2024-07-12 23:53:45 +0200

    Création du modèle Employe

---
+++ app/Models/Employe.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employe extends Model
+{
+    use HasFactory;
+
+    protected $table = 'employe';
+
+    protected $fillable = [
+        'nomEmploye',
+        'prenomEmploye',
+        'telephoneEmploye',
+        'emailEmploye',
+        'adresseEmploye',
+        'codeEmploye',
+        'nationalityEmploye',
+        'dateNaissanceEmploye',
+        'sexeEmploye',
+        'dateEmbaucheEmploye',
+        'photoEmploye',
+        'idFonction',
+        'idSection',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'emailEmploye'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'dateEmbaucheEmploye' => 'datetime',
+        'codeEmploye' => 'hashed',
+    ];
+}
