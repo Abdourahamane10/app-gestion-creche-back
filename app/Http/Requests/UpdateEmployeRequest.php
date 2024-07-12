0164e1aa557339ad4edfbeab85b1b033da9f06c9
Abdourahamane <amdroudjae@gmail.com>
2024-07-13 01:06:12 +0200

    Création de la classe de requête personnalisée pour les mises à jour du modèle Employe

---
+++ app/Http/Requests/UpdateEmployeRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateEmployeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nomEmploye' => ['string', 'required', 'max:50'],
+            'prenomEmploye' => ['string', 'required', 'max:50'],
+            'telephoneEmploye' => ['string', 'max:20'],
+            'emailEmploye' => ['email', 'max:30'],
+            'adresseEmploye' => ['string', 'max:45'],
+            'codeEmploye' => ['string', 'required', 'max:30'],
+            'nationalityEmploye' => ['string', 'max:50'],
+            'dateNaissanceEmploye' => ['date'],
+            'sexeEmploye' => ['string', 'required', 'max:1'],
+            'dateEmbaucheEmploye' => ['date'],
+            'photoEmploye' => ['string', 'max:200'],
+            'idFonction' => ['integer', 'required', 'min:0'],
+            'idSection' => ['integer', 'required', 'min:0']
+        ];
+    }
+}
