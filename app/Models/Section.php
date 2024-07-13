888fe7dffc31ab13cf3bc113a02acb1c180ba71b
Abdourahamane <amdroudjae@gmail.com>
2024-07-13 16:44:37 +0200

    Création du modèle Section

---
+++ app/Models/Section.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Section extends Model
+{
+    use HasFactory;
+
+    protected $table = 'section';
+
+    protected $fillable = [
+        'codeSection',
+        'nomSection'
+    ];
+
+    public function employes()
+    {
+        return $this->hasMany(Employe::class, 'idSection', 'idSection');
+    }
+}
