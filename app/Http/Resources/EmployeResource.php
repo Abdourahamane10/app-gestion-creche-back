90866a3220195ab03b39ae0cc3f86713f669eec5
Abdourahamane <amdroudjae@gmail.com>
2024-07-13 16:19:44 +0200

    Création de la ressource EmployeResource

---
+++ app/Http/Resources/EmployeResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EmployeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->idEmploye,
+            'nom' => $this->nomEmploye,
+            'prenom' => $this->prenomEmploye,
+            'telephone' => $this->telephoneEmploye,
+            'email' => $this->emailEmploye,
+            'adresse' => $this->adresseEmploye,
+            'code' => $this->codeEmploye,
+            'nationality' => $this->nationalityEmploye,
+            'dateNaissance' => $this->dateNaissanceEmploye,
+            'sexe' => $this->sexeEmploye,
+            'dateEmbauche' => $this->dateEmbaucheEmploye,
+            'photo' => $this->photoEmploye,
+            'fonction' => new FonctionResource($this->fonction),
+            'id_section' => new SectionResource($this->section)
+        ];
+    }
+}
