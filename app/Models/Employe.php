c5d71d9ea0a2b221b34127f14bc090a90d48070f
Abdourahamane <amdroudjae@gmail.com>
2024-07-14 16:08:00 +0200

    Implémentation de l'interface JWTSubject et de ses méthodes getJWTIdentifier et getJWTCustomClaims

--- app/Models/Employe.php
+++ app/Models/Employe.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable; // Import correct pour Authenticatable
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Employe extends Model
+class Employe extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $table = 'employe';
 
@@ -55,5 +59,25 @@ class Employe extends Model
     public function section()
     {
         return $this->belongsTo(Section::class, 'idSection', 'idSection');
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
